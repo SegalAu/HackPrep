beaadd8f7f0844d61a20269c9950f665ee45c175
jca248 <jca248@sfu.ca>
2020-04-15 14:58:03 -0700

    count card

--- jsScript.js
+++ jsScript.js
@@ -1,6 +1,7 @@
 
 var deck_id;
 var jsonresponse;
+var count = 1;
 function apiCall() {
     let request = new XMLHttpRequest();
     request.open("GET", "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
@@ -15,6 +16,7 @@ function apiCall() {
             console.log(draw.response);
             var image = JSON.parse(draw.response).cards[0].image;
             document.getElementById("123").src = image;
+            document.getElementById("counter").innerHTML = count++;
         }
     }
 }
